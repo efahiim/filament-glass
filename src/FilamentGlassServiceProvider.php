578c9ce85499132028af81caae570589916a3155
IMF <efahiim@gmail.com>
2025-10-07 12:52:46 +0400

    Fixed publish issues

--- src/FilamentGlassServiceProvider.php
+++ src/FilamentGlassServiceProvider.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace IMFE\FilamentGlass;
 
 use Filament\Support\Assets\Css;
@@ -16,18 +15,31 @@ class FilamentGlassServiceProvider extends PackageServiceProvider
     {
         $package
             ->name(static::$name)
-            ->hasViews();
+            ->hasViews('filament-glass');
+    }
+    
+    public function packageRegistered(): void
+    {
+        // Load views from the package for Livewire components
+        $this->loadViewsFrom($this->package->basePath('/../resources/views'), 'filament-glass');
     }
 
     public function packageBooted(): void
     {
+        // Publish ONLY the regular blade component views to filament-panels vendor directory
         $this->publishes([
             $this->package->basePath('/../resources/views/components/layout') => resource_path('views/vendor/filament-panels/components/layout'),
             $this->package->basePath('/../resources/views/components/sidebar') => resource_path('views/vendor/filament-panels/components/sidebar'),
         ], 'filament-glass-views');
 
+        // Publish CSS assets
         $this->publishes([
             $this->package->basePath('/../resources/css/filament/admin/theme.css') => resource_path('css/filament/admin/theme.css'),
         ], 'filament-glass-assets');
+        
+        // Optionally allow users to publish the livewire views if they want to customize them
+        $this->publishes([
+            $this->package->basePath('/../resources/views/livewire') => resource_path('views/vendor/filament-glass/livewire'),
+        ], 'filament-glass-livewire-views');
     }
 }
